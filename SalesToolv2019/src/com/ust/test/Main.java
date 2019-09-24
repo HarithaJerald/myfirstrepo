b0c730b4b6f87c512ce01712c2cc00b933c1b12c
SYS44 <SYS44@SYS44-PC.faith.com>
2019-09-24 14:56:56 +0530

    initial commit

---
+++ SalesToolv2019/src/com/ust/test/Main.java
@@ -0,0 +1,14 @@
+package com.ust.test;
+
+public class Main {
+	
+	public static void main(String[] args){
+		System.out.println("Welcome to sales app");
+		displayGreeting();
+	}
+	
+	public static void displayGreeting(){
+		System.out.println("Success");
+	}
+
+}
